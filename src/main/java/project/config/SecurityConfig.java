9db188a9aa9f0ab2c03cab5f207b74cba5abf312
yarmail <yarmail@yandex.ru>
2022-10-24 13:22:53 +0300

    lesson 85 + fix

--- src/main/java/project/config/SecurityConfig.java
+++ src/main/java/project/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // временно отключаем csrf токен (защиту от межсайтовой подделки запросов)
+                //.csrf().disable() // временно отключаем csrf токен (защиту от межсайтовой подделки запросов)
                 .authorizeRequests() // начало авторизации -  предоставить разрешения для следующих url
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll() // на эти страниы проходят все
                 .anyRequest().authenticated() // на все остальные страницы мы не пускаем неаутентиф. пользователей
@@ -36,8 +36,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureUrl("/auth/login?error") //после неуспешной аутентиф. перенаправить обратно с ключом
                 .and() // добавляем процесс разлогининивания
                 .logout().logoutUrl("/logout").logoutSuccessUrl("/auth/login"); // ссылка на разлогинивание, автопереход после него
-
-
     }
 
     //Настраиваем аутентификацию используя bcrypt
